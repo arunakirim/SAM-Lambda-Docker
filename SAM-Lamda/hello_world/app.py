a32b2e9eb9e98dccf507ff402141447b19d68d1b
MEIKANDANATHAN Arunakiri <ameikandanathan@mega.com>
2023-02-26 12:15:04 +0800

    Add the V4

--- SAM-Lamda/hello_world/app.py
+++ SAM-Lamda/hello_world/app.py
@@ -7,7 +7,7 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "V2 , This is Arun, Welcome to the AWS learning centre program",
+                "message": "V3 and may be for V4, This is Arun, Welcome to the AWS learning centre program",
             }
         ),
     }
