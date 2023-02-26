049f5ed2a2ac706495fa9584c0be6b33f0fb469b
arunakirim <73383572+arunakirim@users.noreply.github.com>
2023-02-26 12:00:06 +0800

    Update app.py

--- SAM-Lamda/hello_world/app.py
+++ SAM-Lamda/hello_world/app.py
@@ -7,7 +7,7 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "V2 , This is Arunakiri, Welcome to the AWS learning centre program",
+                "message": "V2 , This is Arun, Welcome to the AWS learning centre program",
             }
         ),
     }
